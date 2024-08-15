6252ac10927a335d94895a7e1fda21535fd7f9d6
nucipretty <imelia@student.poltekkesbandung.ac.id>
2024-08-15 14:42:03 +0700

    Add files via upload

---
+++ layers/layers.js
@@ -0,0 +1,74 @@
+var wms_layers = [];
+
+
+        var lyr_GoogleMaps_0 = new ol.layer.Tile({
+            'title': 'Google Maps',
+            //'type': 'base',
+            'opacity': 1.000000,
+            
+            
+            source: new ol.source.XYZ({
+    attributions: ' ',
+                url: 'https://mt1.google.com/vt/lyrs=m&x={x}&y={y}&z={z}'
+            })
+        });
+var format_PetaQGISPraktik_1 = new ol.format.GeoJSON();
+var features_PetaQGISPraktik_1 = format_PetaQGISPraktik_1.readFeatures(json_PetaQGISPraktik_1, 
+            {dataProjection: 'EPSG:4326', featureProjection: 'EPSG:3857'});
+var jsonSource_PetaQGISPraktik_1 = new ol.source.Vector({
+    attributions: ' ',
+});
+jsonSource_PetaQGISPraktik_1.addFeatures(features_PetaQGISPraktik_1);
+var lyr_PetaQGISPraktik_1 = new ol.layer.Vector({
+                declutter: false,
+                source:jsonSource_PetaQGISPraktik_1, 
+                style: style_PetaQGISPraktik_1,
+                popuplayertitle: "Peta QGIS Praktik",
+                interactive: true,
+                title: '<img src="styles/legend/PetaQGISPraktik_1.png" /> Peta QGIS Praktik'
+            });
+var format_Buffered_2 = new ol.format.GeoJSON();
+var features_Buffered_2 = format_Buffered_2.readFeatures(json_Buffered_2, 
+            {dataProjection: 'EPSG:4326', featureProjection: 'EPSG:3857'});
+var jsonSource_Buffered_2 = new ol.source.Vector({
+    attributions: ' ',
+});
+jsonSource_Buffered_2.addFeatures(features_Buffered_2);
+var lyr_Buffered_2 = new ol.layer.Vector({
+                declutter: false,
+                source:jsonSource_Buffered_2, 
+                style: style_Buffered_2,
+                popuplayertitle: "Buffered",
+                interactive: true,
+                title: '<img src="styles/legend/Buffered_2.png" /> Buffered'
+            });
+var format_utmbaru_3 = new ol.format.GeoJSON();
+var features_utmbaru_3 = format_utmbaru_3.readFeatures(json_utmbaru_3, 
+            {dataProjection: 'EPSG:4326', featureProjection: 'EPSG:3857'});
+var jsonSource_utmbaru_3 = new ol.source.Vector({
+    attributions: ' ',
+});
+jsonSource_utmbaru_3.addFeatures(features_utmbaru_3);
+var lyr_utmbaru_3 = new ol.layer.Vector({
+                declutter: false,
+                source:jsonSource_utmbaru_3, 
+                style: style_utmbaru_3,
+                popuplayertitle: "utm baru",
+                interactive: true,
+                title: '<img src="styles/legend/utmbaru_3.png" /> utm baru'
+            });
+
+lyr_GoogleMaps_0.setVisible(true);lyr_PetaQGISPraktik_1.setVisible(true);lyr_Buffered_2.setVisible(true);lyr_utmbaru_3.setVisible(true);
+var layersList = [lyr_GoogleMaps_0,lyr_PetaQGISPraktik_1,lyr_Buffered_2,lyr_utmbaru_3];
+lyr_PetaQGISPraktik_1.set('fieldAliases', {'KDPPUM': 'KDPPUM', 'NAMOBJ': 'NAMOBJ', 'REMARK': 'REMARK', 'KDPBPS': 'KDPBPS', 'FCODE': 'FCODE', 'LUASWH': 'LUASWH', 'UUPP': 'UUPP', 'SRS_ID': 'SRS_ID', 'LCODE': 'LCODE', 'METADATA': 'METADATA', 'KDEBPS': 'KDEBPS', 'KDEPUM': 'KDEPUM', 'KDCBPS': 'KDCBPS', 'KDCPUM': 'KDCPUM', 'KDBBPS': 'KDBBPS', 'KDBPUM': 'KDBPUM', 'WADMKD': 'WADMKD', 'WIADKD': 'WIADKD', 'WADMKC': 'WADMKC', 'WIADKC': 'WIADKC', 'WADMKK': 'WADMKK', 'WIADKK': 'WIADKK', 'WADMPR': 'WADMPR', 'WIADPR': 'WIADPR', 'TIPADM': 'TIPADM', 'SHAPE_Leng': 'SHAPE_Leng', 'SHAPE_Area': 'SHAPE_Area', });
+lyr_Buffered_2.set('fieldAliases', {'Name': 'Name', 'descriptio': 'descriptio', 'timestamp': 'timestamp', 'begin': 'begin', 'end': 'end', 'altitudeMo': 'altitudeMo', 'tessellate': 'tessellate', 'extrude': 'extrude', 'visibility': 'visibility', 'drawOrder': 'drawOrder', 'icon': 'icon', });
+lyr_utmbaru_3.set('fieldAliases', {'Name': 'Name', 'descriptio': 'descriptio', 'timestamp': 'timestamp', 'begin': 'begin', 'end': 'end', 'altitudeMo': 'altitudeMo', 'tessellate': 'tessellate', 'extrude': 'extrude', 'visibility': 'visibility', 'drawOrder': 'drawOrder', 'icon': 'icon', });
+lyr_PetaQGISPraktik_1.set('fieldImages', {'KDPPUM': 'TextEdit', 'NAMOBJ': 'TextEdit', 'REMARK': 'TextEdit', 'KDPBPS': 'TextEdit', 'FCODE': 'TextEdit', 'LUASWH': 'TextEdit', 'UUPP': 'TextEdit', 'SRS_ID': 'TextEdit', 'LCODE': 'TextEdit', 'METADATA': 'TextEdit', 'KDEBPS': 'TextEdit', 'KDEPUM': 'TextEdit', 'KDCBPS': 'TextEdit', 'KDCPUM': 'TextEdit', 'KDBBPS': 'TextEdit', 'KDBPUM': 'TextEdit', 'WADMKD': 'TextEdit', 'WIADKD': 'TextEdit', 'WADMKC': 'TextEdit', 'WIADKC': 'TextEdit', 'WADMKK': 'TextEdit', 'WIADKK': 'TextEdit', 'WADMPR': 'TextEdit', 'WIADPR': 'TextEdit', 'TIPADM': 'Range', 'SHAPE_Leng': 'TextEdit', 'SHAPE_Area': 'TextEdit', });
+lyr_Buffered_2.set('fieldImages', {'Name': 'TextEdit', 'descriptio': 'TextEdit', 'timestamp': 'TextEdit', 'begin': 'TextEdit', 'end': 'TextEdit', 'altitudeMo': 'TextEdit', 'tessellate': 'TextEdit', 'extrude': 'TextEdit', 'visibility': 'TextEdit', 'drawOrder': 'TextEdit', 'icon': 'TextEdit', });
+lyr_utmbaru_3.set('fieldImages', {'Name': 'TextEdit', 'descriptio': 'TextEdit', 'timestamp': 'TextEdit', 'begin': 'TextEdit', 'end': 'TextEdit', 'altitudeMo': 'TextEdit', 'tessellate': 'TextEdit', 'extrude': 'TextEdit', 'visibility': 'TextEdit', 'drawOrder': 'TextEdit', 'icon': 'TextEdit', });
+lyr_PetaQGISPraktik_1.set('fieldLabels', {'KDPPUM': 'no label', 'NAMOBJ': 'no label', 'REMARK': 'no label', 'KDPBPS': 'no label', 'FCODE': 'no label', 'LUASWH': 'no label', 'UUPP': 'no label', 'SRS_ID': 'no label', 'LCODE': 'no label', 'METADATA': 'no label', 'KDEBPS': 'no label', 'KDEPUM': 'no label', 'KDCBPS': 'no label', 'KDCPUM': 'no label', 'KDBBPS': 'no label', 'KDBPUM': 'no label', 'WADMKD': 'no label', 'WIADKD': 'no label', 'WADMKC': 'no label', 'WIADKC': 'no label', 'WADMKK': 'no label', 'WIADKK': 'no label', 'WADMPR': 'no label', 'WIADPR': 'no label', 'TIPADM': 'no label', 'SHAPE_Leng': 'no label', 'SHAPE_Area': 'no label', });
+lyr_Buffered_2.set('fieldLabels', {'Name': 'no label', 'descriptio': 'no label', 'timestamp': 'no label', 'begin': 'no label', 'end': 'no label', 'altitudeMo': 'no label', 'tessellate': 'no label', 'extrude': 'no label', 'visibility': 'no label', 'drawOrder': 'no label', 'icon': 'no label', });
+lyr_utmbaru_3.set('fieldLabels', {'Name': 'no label', 'descriptio': 'no label', 'timestamp': 'no label', 'begin': 'no label', 'end': 'no label', 'altitudeMo': 'no label', 'tessellate': 'no label', 'extrude': 'no label', 'visibility': 'no label', 'drawOrder': 'no label', 'icon': 'no label', });
+lyr_utmbaru_3.on('precompose', function(evt) {
+    evt.context.globalCompositeOperation = 'normal';
+});
